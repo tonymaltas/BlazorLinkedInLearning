2d86d9b2e7e3f79f79eff010d0cdd604e993e7a0
tonym <tonymaltas@yahoo.co.uk>
2020-09-23 18:09:37 +0100

    JS animations

--- Beam.Client/Program.cs
+++ Beam.Client/Program.cs
@@ -26,6 +26,8 @@ namespace Beam.Client
             builder.Services.AddScoped<AuthenticationStateProvider>
                 (s => s.GetRequiredService<BeamAuthenticationStateProvider>());
             builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddSingleton<AnimationService>();
+
             await builder.Build().RunAsync();
         }
     }
---
+++ Beam.Client/Services/AnimationService.cs
@@ -0,0 +1,28 @@
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace Beam.Client.Services
+{
+    public class AnimationService
+    {
+        private IJSRuntime _jsRuntime;
+        public static event Action BeamPassTriggered;
+
+        public AnimationService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public ValueTask LoadAnimation(string elementId, int width, int height)
+        {
+            return _jsRuntime.InvokeVoidAsync("animatedBeam.loadAnimation", elementId, width, height);
+        }
+
+        [JSInvokable]
+        public static Task BeamPassedBy()
+        {
+            return Task.Run(() => BeamPassTriggered?.Invoke());
+        }
+    }
+}
